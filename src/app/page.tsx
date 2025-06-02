bebdf61831678959347e6125f65d4d146c7606b5
devsambini <luis.sambini@gmail.com>
2025-06-02 19:42:19 -0300

    apagando o swiper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,6 @@ import { Galery } from "./_components/galery"
 import { Pastor } from "./_components/pastor"
 import { NextEvents } from "./_components/nextEvents"
 import { Footer } from "./_components/footer"
-import { register } from "swiper/element/bundle"
-
-register()
-import 'swiper/css'
-import 'swiper/css/navigation'
-import 'swiper/css/pagination'
-import 'swiper/css/scrollbar'
 
 
 export default function Home(){
